dfdff46abc313b515f1bfb4fe5842e3a956a65f7
Gabe <gabenedden@gmail.com>
2021-10-07 10:54:50 -0400

    Add constant routes, Add Header

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
 import { JumbotronContainer } from '../containers/jumbotron';
+import { HeaderContainer } from '../containers/header';
 
 export default function Home() {
     return (
         <>
+        <HeaderContainer>
+            <p>Hello</p>
+        </HeaderContainer>
         <JumbotronContainer />
         <FaqsContainer />
         <FooterContainer />
